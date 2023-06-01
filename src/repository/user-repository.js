e138704133234e12dd7887c24355a472bdcb552f
ChampionVikash <4355vikashkumar4355@gmail.com>
2023-06-01 22:12:16 +0530

    Added get user by id in repo

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -23,7 +23,19 @@ class UserRepository {
              console.log("Something went wrong on repository layer");
              throw error;
        }
-  }
+     }
+     
+     async getById(userId) {
+         try {
+              const user = await User.findByPk(userId, {
+                   attributes: ['email', 'id']
+              });
+              return user;
+         } catch (error) {
+              console.log("Something went wrong on repository layer");
+              throw error;
+         }
+     }
 }
 
 module.exports = UserRepository;
