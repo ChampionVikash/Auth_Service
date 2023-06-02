ad87f9968da24771d8b74fe85a8279135ffd5b91
ChampionVikash <4355vikashkumar4355@gmail.com>
2023-06-02 15:58:31 +0530

    Added token gen logic from sign in

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -25,17 +25,30 @@ class UserRepository {
        }
      }
      
-     async getById(userId) {
-         try {
-              const user = await User.findByPk(userId, {
-                   attributes: ['email', 'id']
-              });
-              return user;
-         } catch (error) {
+ async getById(userId) {
+      try {
+           const user = await User.findByPk(userId, {
+                attributes: ['email', 'id']
+            });
+           return user;
+      } catch (error) {
               console.log("Something went wrong on repository layer");
               throw error;
-         }
-     }
+      }
+ }
+
+ async getByEmail(userEmail) {
+      try {
+           const user = await User.findOne({
+                where: { 
+                     email: userEmail
+                }});
+           return user;
+      } catch (error) {
+           console.log("Something went wrong on repository layer");
+           throw error; 
+      }
+ }
 }
 
 module.exports = UserRepository;
