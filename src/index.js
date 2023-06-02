156b33732b59e215eda13f83a7dd8ad003f26dde
ChampionVikash <4355vikashkumar4355@gmail.com>
2023-06-02 10:17:54 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-// const { User } = require('./models/index');
-// const bcrypt = require('bcrypt');
-// const UserRepository = require('./repository/user-repository');
-
 
 const app = express();
 
@@ -20,13 +16,7 @@ const prepareAndStartServer = () => {
 
   app.listen(PORT, async () => {
     console.log(`Server Started on Port : ${PORT}`);
-    // const repo = new UserRepository();
-    // const response = await repo.getById(1);
-    // console.log(response);
-    // const incomingpassword = '987654';
-    // const user = await User.findByPk(3);
-    // const response = bcrypt.compareSync(incomingpassword, user.password);
-    // console.log(response);
+    
   }); 
 } 
 
