1fb34cb930ad9b2c034af3391706f1619a715ec2
ChampionVikash <4355vikashkumar4355@gmail.com>
2023-06-03 16:20:57 +0530

    Added isadmin api and validation error class

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -1,5 +1,7 @@
 const { ValidationError } = require('../utils/validation-error');
 const { User,Role } = require('../models/index');
+const { StatusCodes } = require('http-status-codes');
+const ClientError = require('../utils/client-error');
 
 class UserRepository {
    
@@ -47,8 +49,17 @@ class UserRepository {
                 where: { 
                      email: userEmail
                 }});
+           if (!user) {
+                throw new ClientError(
+                     'AttributeNotFound',
+                     'Invalid email sent tn the request',
+                     'Please check the email,as there is no record of the email',
+                     StatusCodes.NOT_FOUND     
+                );
+           }
            return user;
       } catch (error) {
+           console.log(error);
            console.log("Something went wrong on repository layer");
            throw error; 
       }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -38,6 +38,9 @@ class UserService {
             const newJWT = this.createToken({ email: user.email, id: user.id });
             return newJWT;
         } catch (error) {
+            if (error.name == 'AttributeNotFound') {
+                throw error;
+            }
             console.log("Something went wrong in the sign in process");
             throw error;
         }
